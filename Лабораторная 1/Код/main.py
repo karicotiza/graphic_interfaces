11344f0744c65e0e57cb63d819ace586c4fbcef8
karicotiza <Kur7umerovD@Gmail.com>
2022-09-17 23:34:55 +0300

    Рефакторинг

--- Лабораторная 1/Код/main.py
+++ Лабораторная 1/Код/main.py
@@ -1,60 +1,26 @@
-import dataclasses
-import numpy as np
-from PIL import Image
-
-
-class Gamut:
-    data_type = np.uint8
-
-
-@dataclasses.dataclass
-class Point:
-    x: int
-    y: int
-
-
-@dataclasses.dataclass
-class Colour(Gamut):
-    value: int
-
-    def __post_init__(self):
-        if not (np.iinfo(super().data_type).min <= self.value <= np.iinfo(super().data_type).max):
-            raise ValueError(
-                f"Value must be between {np.iinfo(Gamut.data_type).min} and {np.iinfo(Gamut.data_type).max}"
-            )
-        self.value = super().data_type(self.value)
-
-    def get_value(self):
-        return self.value
-
-
-class Board:
-    def __init__(self, x: int, y: int, colour: Colour) -> None:
-        self.__layer = np.full(
-            shape=(x, y),
-            fill_value=colour.get_value(),
-            dtype=np.uint8,
-        )
-
-    def get_as_numpy_array(self) -> np.array:
-        return self.__layer
-
-    def draw_line(self, start_point: Point, end_point: Point, colour: Colour):
-        self.__paint(start_point, colour)
-        self.__paint(end_point, colour)
-
-    def __paint(self, point: Point, colour: Colour) -> None:
-        self.__layer[point.x][point.y] = colour.get_value()
-
+import canvas
+from random import randint
 
 if __name__ == "__main__":
-    board = Board(50, 50, Colour(255))
-    board.draw_line(
-        Point(10, 10),
-        Point(10, 25),
-        Colour(0)
+    layer = canvas.Layer(
+        height=canvas.Dimension(50),
+        width=canvas.Dimension(50),
+        colour=canvas.Colour(255, 255, 255)
     )
 
-    image = Image.fromarray(board.get_as_numpy_array())
-    image.show(title="image")
-    image.save("image.png")
+    for _ in range(500):
+        layer = canvas.Brush.plus(
+            layer,
+            canvas.Point(
+                randint(1, layer.height - 2),
+                randint(1, layer.width - 2),
+            ),
+            canvas.Colour(
+                randint(0, 255),
+                randint(0, 255),
+                randint(0, 255),
+            )
+        )
+
+    layer.show()
+    layer.save("image.png")
